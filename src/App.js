36748ba3e716ba4f537029b73bf68bf34706e6a3
reinaldoperes <reinaldoporte@hotmail.com>
2020-06-01 08:29:55 -0300

    commit inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import './style.css'
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            numero: 0,
+            botao: 'INICIAR'
+        };
+
+        this.timer = null
+        this.iniciar = this.iniciar.bind(this);
+        this.limpar = this.limpar.bind(this);
+    }
+
+    iniciar(){
+        let state = this.state;
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botao = 'INICIAR';
+        }
+        else{
+            this.timer = setInterval(()=>{
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state);
+            },100)
+            state.botao = 'PAUSAR';
+        }
+        this.setState(this.state);
+    }
+
+        
+
+    limpar(){
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+        let state = this.state;
+        state.numero = 0;
+        state.botao = 'INICIAR';
+        this.setState(state);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <img src={require('./assets/cronometro.png')} className="img"/>
+                <a className="timer">{this.state.numero.toFixed(1)}</a>
+                <div className="areaBtn">
+                    <a className="botao" onClick={this.iniciar}>{this.state.botao}</a>
+                    <a className="botao" onClick={this.limpar}>ZERAR</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
